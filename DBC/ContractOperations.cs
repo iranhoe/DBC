db93fab279a1fb6df31c7446b60bc6bcdf162cd3
Iran Corrales Becerra <iranhoe@live.com.mx>
2019-02-11 21:34:24 -0600

    change name to NContracts

--- DBC/ContractOperations.cs
+++ DBC/ContractOperations.cs
@@ -1,20 +1,20 @@
-﻿// <copyright file="ContractOperations.cs" company="Iran Corrales Becerra">
+﻿// <copyright file="StringConstants.cs" company="Iran Corrales Becerra">
 //    This file is part of DBC library.
 //
-//    DBC library is free software: you can redistribute it and/or modify
+//    NContracts library is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    DBC library is distributed in the hope that it will be useful,
+//    NContracts library is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with DBC library.  If not, see https://www.gnu.org/licenses/
+//    along with NContracts library.  If not, see https://www.gnu.org/licenses/
 // </copyright>
-namespace DBC
+namespace NContracts
 {
     using System;
 
